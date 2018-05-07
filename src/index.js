4bfce24c8d812ddb3955022addeb30a4fafc7a90
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-05-07 15:32:32 +0100

    serviceWorker: implemented

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('___root'));
+
+registerServiceWorker();
